00f8a45c17b24ebc4eafdc83471adcd94011f3de
CodeMasterCody3D <130433943+CodeMasterCody3D@users.noreply.github.com>
2024-08-05 08:36:27 -0500

    Update rick.sh

--- rick.sh
+++ rick.sh
@@ -4,11 +4,19 @@
 TMP_DIR="$HOME/RickPi/tmp_files"
 LOG_FILE="/tmp/rife_transmission.log"
 RPITX_PATH="$HOME/rpitx"
-OUTPUT_FREQ=31000000
+DEFAULT_OUTPUT_FREQ=434.0
 GAIN_AMPLITUDE=8.0
+SAMPLE_RATE=48000
+DEFAULT_WAV_FILE_STEREO_LOC="$HOME/RickPi/sample_stereo_audio.wav"
 
 # Ensure the tmp_files directory exists
 mkdir -p "$TMP_DIR"
+
+# Function to choose the transmission frequency
+choose_output_frequency() {
+    OUTPUT_FREQ=$(whiptail --inputbox "Enter output Frequency (in MHz). Default is 434 MHz" 8 78 $DEFAULT_OUTPUT_FREQ --title "Rpitx transmit Frequency" 3>&1 1>&2 2>&3)
+    OUTPUT_FREQ=${OUTPUT_FREQ:-$DEFAULT_OUTPUT_FREQ}
+}
 
 # List imported WAV files
 list_imported_wav_files() {
@@ -33,13 +41,12 @@ transmit_files() {
     echo "while true; do" >> $TRANSMIT_SCRIPT
 
     while IFS= read -r wav_path; do
-        sample_rate=$(soxi -r "$wav_path")
         duration=$(soxi -D "$wav_path")
 
-        echo "echo 'Transmitting \$(basename $wav_path .wav) using $wav_path at sample rate $sample_rate' | tee -a $LOG_FILE" >> $TRANSMIT_SCRIPT
-        echo "screen -dmS rpitx_session bash -c \"cat '$wav_path' | csdr convert_i16_f | csdr gain_ff $GAIN_AMPLITUDE | csdr dsb_fc | sudo $RPITX_PATH/rpitx -i - -m IQFLOAT -f '$OUTPUT_FREQ' -s $sample_rate\"" >> $TRANSMIT_SCRIPT
+        echo "echo 'Transmitting \$(basename $wav_path .wav) using $wav_path at sample rate $SAMPLE_RATE' | tee -a $LOG_FILE" >> $TRANSMIT_SCRIPT
+        echo "screen -dmS rpitx_session bash -c \"sudo $RPITX_PATH/pifmrds -freq $OUTPUT_FREQ -audio $wav_path\"" >> $TRANSMIT_SCRIPT
         echo "sleep $duration" >> $TRANSMIT_SCRIPT
-        echo "sudo killall rpitx" >> $TRANSMIT_SCRIPT
+        echo "sudo killall pifmrds" >> $TRANSMIT_SCRIPT
         echo "sleep 1" >> $TRANSMIT_SCRIPT
     done < "$TMP_DIR/generated_files.txt"
 
@@ -50,4 +57,5 @@ transmit_files() {
 }
 
 # Main script execution
+choose_output_frequency
 list_imported_wav_files
